db0fb4b1d760a5bb426e26d7c93ca47ff27a221b
JeffreyChen <33644111+JE-Chen@users.noreply.github.com>
2023-05-08 13:08:41 +0800

    Now we can show on all screen
    Now we can show on all screen

--- frontengine/ui/setting/web/web_setting_ui.py
+++ frontengine/ui/setting/web/web_setting_ui.py
@@ -1,4 +1,5 @@
 from PySide6.QtCore import Qt
+from PySide6.QtGui import QScreen
 from PySide6.QtWidgets import QWidget, QGridLayout, QLabel, QSlider, QLineEdit, QPushButton
 
 from frontengine.show.web.webview import WebWidget
@@ -37,13 +38,25 @@ class WEBSettingUI(QWidget):
         self.grid_layout.addWidget(self.web_url_input, 1, 2)
         self.setLayout(self.grid_layout)
 
-    def opacity_trick(self):
-        self.opacity_slider_value_label.setText(str(self.opacity_slider.value()))
-
-    def start_open_web(self):
+    def _create_web_widget(self):
         web_widget = WebWidget(
             self.web_url_input.text(),
             float(self.opacity_slider.value()) / 100
         )
         self.web_widget_list.append(web_widget)
-        web_widget.showMaximized()
+        return web_widget
+
+    def opacity_trick(self):
+        self.opacity_slider_value_label.setText(str(self.opacity_slider.value()))
+
+    def start_open_web(self):
+        if self.show_all_screen:
+            web_widget = self._create_web_widget()
+            web_widget.showMaximized()
+        else:
+            monitors = QScreen.virtualSiblings(self.screen())
+            for screen in monitors:
+                monitor = screen.availableGeometry()
+                web_widget = self._create_web_widget()
+                web_widget.move(monitor.left(), monitor.top())
+                web_widget.showMaximized()
