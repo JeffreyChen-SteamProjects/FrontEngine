0c78482033feffd1902108c410c959878df24084
JeffreyChen <33644111+JE-Chen@users.noreply.github.com>
2023-05-10 14:49:30 +0800

    Refactor and add ui style choose
    Refactor and add ui style choose

--- frontengine/ui/main/main_ui.py
+++ frontengine/ui/main/main_ui.py
@@ -2,9 +2,9 @@ import os
 import sys
 from pathlib import Path
 
-from PySide6.QtGui import QIcon
-from PySide6.QtWidgets import QMainWindow, QApplication, QGridLayout, QTabWidget, QSystemTrayIcon
-from qt_material import apply_stylesheet
+from PySide6.QtGui import QIcon, QAction
+from PySide6.QtWidgets import QMainWindow, QApplication, QGridLayout, QTabWidget, QSystemTrayIcon, QMenuBar
+from qt_material import apply_stylesheet, QtStyleTools
 
 from frontengine.ui.setting.gif.gif_setting_ui import GIFSettingUI
 from frontengine.ui.setting.image.image_setting_ui import ImageSettingUI
@@ -14,7 +14,7 @@ from frontengine.ui.setting.video.video_setting_ui import VideoSettingUI
 from frontengine.ui.setting.web.web_setting_ui import WEBSettingUI
 
 
-class FrontEngineMainUI(QMainWindow):
+class FrontEngineMainUI(QMainWindow, QtStyleTools):
 
     def __init__(self):
         super().__init__()
@@ -47,6 +47,25 @@ class FrontEngineMainUI(QMainWindow):
             self.setWindowIcon(self.icon)
             self.system_icon = QSystemTrayIcon()
             self.system_icon.setIcon(self.icon)
+        # Style menu bar
+        self.menu_bar = QMenuBar()
+        self.add_style_menu()
+        self.setMenuBar(self.menu_bar)
+
+    def add_style_menu(self):
+        self.menu_bar.style_menu = self.menu_bar.addMenu("UI Style")
+        for style in [
+            'dark_amber.xml', 'dark_blue.xml', 'dark_cyan.xml', 'dark_lightgreen.xml', 'dark_pink.xml',
+            'dark_purple.xml', 'dark_red.xml', 'dark_teal.xml', 'dark_yellow.xml', 'light_amber.xml',
+            'light_blue.xml', 'light_cyan.xml', 'light_cyan_500.xml', 'light_lightgreen.xml',
+            'light_pink.xml', 'light_purple.xml', 'light_red.xml', 'light_teal.xml', 'light_yellow.xml'
+        ]:
+            change_style_action = QAction(style, parent=self)
+            change_style_action.triggered.connect(self.set_style)
+            self.menu_bar.style_menu.addAction(change_style_action)
+
+    def set_style(self):
+        self.apply_stylesheet(self, self.sender().text())
 
     def closeEvent(self, event) -> None:
         super().closeEvent(event)
