14b18b7447e04a72ddc55046648c4aec676c2275
JE-Chen <33644111+JE-Chen@users.noreply.github.com>
2024-05-01 22:15:37 +0800

    Extract key press check from je_auto_control
    Extract key press check from je_auto_control

--- frontengine/utils/critical_exit/critical_exit.py
+++ frontengine/utils/critical_exit/critical_exit.py
@@ -1,12 +1,34 @@
 import _thread
 import sys
+from threading import Thread
 
-from PySide6.QtWidgets import QApplication
-from je_auto_control import CriticalExit
-from je_auto_control.wrapper.platform_wrapper import keyboard_check
+from frontengine.utils.critical_exit.check_key_is_press import check_key_is_press
+from frontengine.utils.critical_exit.win32_vk import keyboard_keys_table
 
 
-class FrontEngineCriticalExit(CriticalExit):
+class CriticalExit(Thread):
+    """
+    use to make program interrupt
+    """
+
+    def __init__(self, default_daemon: bool = True):
+        """
+        default interrupt is keyboard F7 key
+        :param default_daemon bool thread setDaemon
+        """
+        super().__init__()
+        self.daemon = default_daemon
+        self._exit_check_key: int = keyboard_keys_table.get("f7")
+
+    def set_critical_key(self, keycode: [int, str] = None) -> None:
+        """
+        set interrupt key
+        :param keycode interrupt key
+        """
+        if isinstance(keycode, int):
+            self._exit_check_key = keycode
+        else:
+            self._exit_check_key = keyboard_keys_table.get(keycode)
 
     def run(self) -> None:
         """
@@ -14,8 +36,15 @@ class FrontEngineCriticalExit(CriticalExit):
         """
         try:
             while True:
-                if keyboard_check.check_key_is_press(self._exit_check_key):
+                if check_key_is_press(self._exit_check_key):
                     _thread.interrupt_main()
-                    QApplication.exit(0)
         except Exception as error:
             print(repr(error), file=sys.stderr)
+
+    def init_critical_exit(self) -> None:
+        """
+        should only use this to start critical exit
+        may this function will add more
+        """
+        critical_thread = self
+        critical_thread.start()
