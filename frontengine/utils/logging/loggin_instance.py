f1565592e2782cedb8bd0e20ad2220bafa4fcd3c
JE-Chen <33644111+JE-Chen@users.noreply.github.com>
2024-12-08 04:37:20 +0800

    Update dev version
    Update dev version
    * Update logger

--- frontengine/utils/logging/loggin_instance.py
+++ frontengine/utils/logging/loggin_instance.py
@@ -1,15 +1,25 @@
 import logging
-import sys
 
+logging.root.setLevel(logging.DEBUG)
 front_engine_logger = logging.getLogger("FrontEngine")
-front_engine_logger.setLevel(logging.INFO)
 formatter = logging.Formatter('%(asctime)s | %(name)s | %(levelname)s | %(message)s')
-# Stream handler
-stream_handler = logging.StreamHandler(stream=sys.stderr)
-stream_handler.setFormatter(formatter)
-stream_handler.setLevel(logging.WARNING)
-front_engine_logger.addHandler(stream_handler)
 # File handler
 file_handler = logging.FileHandler(filename="FrontEngine.log", mode="w")
 file_handler.setFormatter(formatter)
 front_engine_logger.addHandler(file_handler)
+
+class FrontEngineLoggingHandler(logging.Handler):
+
+    # redirect logging stderr output to queue
+
+    def __init__(self):
+        super().__init__()
+        self.formatter = formatter
+        self.setLevel(logging.DEBUG)
+
+    def emit(self, record: logging.LogRecord) -> None:
+        print(self.format(record))
+
+
+# Stream handler
+front_engine_logger.addHandler(FrontEngineLoggingHandler())
