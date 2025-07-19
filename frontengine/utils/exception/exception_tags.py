1cb104a9e20c31102b3cb872b52567f07f6a40c0
JE-Chen <33644111+JE-Chen@users.noreply.github.com>
2025-07-19 17:43:09 +0800

    Update both version
    Update exception tags

--- frontengine/utils/exception/exception_tags.py
+++ frontengine/utils/exception/exception_tags.py
@@ -1,11 +1,11 @@
 """
 json exception
 """
-cant_reformat_json_error: str = "Can't reformat json is type right?"
-wrong_json_data_error: str = "Can't parser json"
-cant_find_json_error: str = "cant find json file"
-cant_save_json_error: str = "cant save json file"
+cant_reformat_json_error: str = "Cannot reformat JSON: is the type correct?"
+wrong_json_data_error: str = "Cannot parse JSON"
+cant_find_json_error: str = "Cannot find JSON file"
+cant_save_json_error: str = "Cannot save JSON file"
 """
 language
 """
-language_setting_error: str = "Setting language failed"
+language_setting_error: str = "Failed to set language"
